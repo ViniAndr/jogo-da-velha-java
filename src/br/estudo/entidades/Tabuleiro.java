b3adb592db9c002e51ddfefe800a0122ab24ca33
Vinicius Andrade <fabioviniciusbarbosa@hotmail.com>
2023-10-03 09:55:00 -0300

    criação da logica do vencendor, jogador da vez, e pequenas correções

--- src/br/estudo/entidades/Tabuleiro.java
+++ src/br/estudo/entidades/Tabuleiro.java
@@ -2,9 +2,19 @@ package br.estudo.entidades;
 
 public class Tabuleiro {
 	private char[][] tabuleiro = new char[3][3];
-	
+	private char jogadorAtual = 'X';
+	private boolean posicaoOk = false;
+
 	public Tabuleiro() {
 		criarTabuleiro();
+	}
+
+	public boolean getPosicaoOk() {
+		return posicaoOk;
+	}
+	
+	public char getJogadorAtual() {
+		return jogadorAtual;
 	}
 
 	public void criarTabuleiro() {
@@ -23,9 +33,51 @@ public class Tabuleiro {
 			System.out.println();
 		}
 	}
-	
+
+	public void trocarJogador() {
+		jogadorAtual = (jogadorAtual == 'X') ? 'O' : 'X';
+	}
+
 	public void receberPosicao(int linha, int coluna) {
-		tabuleiro[linha][coluna] = 'X';
+		if (tabuleiro[linha][coluna] == '-') {
+			if (linha < 3 && coluna < 3) {
+				posicaoOk = true;
+				colocarNaPosicao(linha, coluna);
+			} else
+				System.out.println("Digite uma posição menor que o tamanho do tabuleiro");
+		} else
+			System.out.println("Digite uma posição que ainda não usada");
+	}
+
+	public void colocarNaPosicao(int linha, int coluna) {
+		tabuleiro[linha][coluna] = jogadorAtual;
+		trocarJogador();
 		mostrarTabela();
+	}
+
+	public char verificarVencedor() {
+		// verificar na linha
+		for (int linha = 0; linha < 3; linha++) {
+			if (tabuleiro[linha][0] == tabuleiro[linha][1] && tabuleiro[linha][1] == tabuleiro[linha][2]
+					&& tabuleiro[linha][0] != '-')
+				return tabuleiro[linha][0];
+		}
+
+		// verificar se foi na coluna
+		for (int coluna = 0; coluna < 3; coluna++) {
+			if (tabuleiro[0][coluna] == tabuleiro[1][coluna] && tabuleiro[1][coluna] == tabuleiro[2][coluna]
+					&& tabuleiro[0][coluna] != '-')
+				return tabuleiro[0][coluna];
+		}
+
+		// vericar na diagonal da esquerda para direita
+		if (tabuleiro[0][0] == tabuleiro[1][1] && tabuleiro[1][1] == tabuleiro[2][2] && tabuleiro[0][0] != '-')
+			return tabuleiro[0][0];
+
+		// vericar na diagonal da direita para esquerda
+		if (tabuleiro[0][2] == tabuleiro[1][1] && tabuleiro[1][1] == tabuleiro[2][0] && tabuleiro[0][2] != '-')
+			return tabuleiro[0][2];
+
+		return '-';
 	}
 }
--- src/br/estudo/principal/Main.java
+++ src/br/estudo/principal/Main.java
@@ -9,9 +9,18 @@ public class Main {
 	public static void main(String[] args) {
 		Tabuleiro tabuleiro = new Tabuleiro();
 		Scanner in = new Scanner(System.in);
-		int linha = in.nextInt();
-		int coluna = in.nextInt();
-		tabuleiro.receberPosicao(linha, coluna);
+
+		do {
+			System.out.println("Jogador " + tabuleiro.getJogadorAtual() + ", insira a linha (0-2) e a coluna (0-2): ");
+			int linha = in.nextInt();
+			int coluna = in.nextInt();
+			tabuleiro.receberPosicao(linha, coluna);
+			if (tabuleiro.verificarVencedor() != '-') {
+				System.out.println("Jogador " + tabuleiro.getJogadorAtual() + "Ganhou a rodada");
+				break;
+			}
+		} while (tabuleiro.getPosicaoOk());
+
 		in.close();
 	}
 
